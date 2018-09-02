8b3b572d34a2a339a351e7f63595d70bddaf3c42
takahirosawamura <cawst222@gmail.com>
2018-09-02 22:57:46 +0900

    add:ropsten deploy

--- truffle.js
+++ truffle.js
@@ -1,18 +1,20 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+require('dotenv').config({path: '.env.local'});
+const Web3 = require("web3");
+const web3 = new Web3();
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require('ethereumjs-wallet');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+ networks: {
+  ropsten: {
+      provider: function(){
+       var ropstenPrivateKey = new Buffer(process.env["ROPSTEN_PRIVATE_KEY"], "hex")
+    var ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+       return new WalletProvider(ropstenWallet, "https://ropsten.infura.io/");
+      },
+      gas: 4600000,
+        gasPrice: web3.toWei("20", "gwei"),
+      network_id: '3',
+  }
+ }
 };
